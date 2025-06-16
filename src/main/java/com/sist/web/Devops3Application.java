25f8f82603208da149ce708d8795ab537efb2c78
woogy9999 <gusdnrdkshf@gmail.com>
2025-06-16 14:50:14 +0900

    dev

---
+++ src/main/java/com/sist/web/Devops3Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops3Application.class, args);
+	}
+
+}
